6dccabe958689977882978b94da4cec5640ce2bd
akshch <akshaych2786@gmail.com>
2020-03-12 23:47:48 +0530

    game location helper method added

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  def get_game_locations(game)
+    game_locations = []
+    game.locations.each do |location|
+      game_locations << location.name
+    end
+    return game_locations.join(",")
+  end
 end
